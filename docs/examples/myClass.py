3711cfba44570075dc1c0174a861d788c8afc28d
daquinteroflex <darioaquintero@gmail.com>
2024-07-25 17:53:24 +0100

    :shipit: Old functionality

---
+++ docs/examples/myClass.py
@@ -0,0 +1,27 @@
+import pydantic
+
+
+class MyClass(pydantic.BaseModel):
+    my_parameter_1: int = 1
+    my_parameter_2: str = 2
+    my_parameter_3: float = 3.0
+
+    def my_method(self):
+        """
+        This is my method.
+        """
+        pass
+
+    @classmethod
+    def my_class_method(cls):
+        """
+        This is my class method.
+        """
+        pass
+
+    @staticmethod
+    def my_static_method():
+        """
+        This is my static method.
+        """
+        pass
